bcd1e62d25daadc2a63f4ce01b7f6e2fe8010c77
nagiya0213-art <nagiya0213@gmail.com>
2026-02-21 14:51:45 +0900

    simple roster version

--- server.js
+++ server.js
@@ -1,24 +1,3 @@
-const express = require("express");
-const axios = require("axios");
-const cheerio = require("cheerio");
-
-const app = express();
-
-const teams = {
-  g: "giants",
-  t: "tigers",
-  d: "dragons",
-  c: "carp",
-  db: "baystars",
-  s: "swallows",
-  l: "lions",
-  h: "hawks",
-  e: "eagles",
-  m: "marines",
-  f: "fighters",
-  b: "buffaloes"
-};
-
 app.get("/", async (req, res) => {
   const teamCode = req.query.team;
 
@@ -33,15 +12,12 @@ app.get("/", async (req, res) => {
 
     const players = [];
 
-    $("table tr").each((i, el) => {
-      const tds = $(el).find("td");
-      if (tds.length >= 2) {
-        const number = tds.eq(0).text().trim();
-        const name = tds.eq(1).text().trim();
+    $("tr").each((i, el) => {
+      const number = $(el).find("td").eq(0).text().trim();
+      const name = $(el).find("td").eq(1).text().trim();
 
-        if (/^\d+$/.test(number) && name) {
-          players.push({ number, name });
-        }
+      if (number && /^\d+$/.test(number) && name) {
+        players.push({ number, name });
       }
     });
 
@@ -61,8 +37,4 @@ app.get("/", async (req, res) => {
     console.error(err.message);
     res.send("選手データ取得失敗");
   }
-});
-
-app.listen(3000, () => {
-  console.log("Server started");
 });
